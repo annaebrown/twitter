45746a807e37d2ebfed533702f5f774484687573
annaebrown <annabrown4789@gmail.com>
2016-11-07 16:59:55 -0500

    hehe

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require( 'express' );
 var app = express(); 
 
+app.use(function(req, res, next) {
+	console.log('logged')
+	next();
+})
+
 app.get('/', function(req, res) {
 	res.send("we are golden!");
+})
+
+app.get('/news', function(req, res){
+	res.send("word")
 })
 
 app.listen(3000, function(){
 	console.log("we are golden")
 })
+
