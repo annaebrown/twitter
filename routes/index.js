07643c82f9f5cbc589945f1547c4b4de1eab8ce8
Chloe Rice <chloe.mi.rice@gmail.com>
2016-11-08 14:32:18 -0500

    up to step 8 of workshop

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,16 @@ router.get('/', function (req, res) {
   res.render( 'index', { tweets: tweets } );
 });
 
+router.get('/users/:name', function(req, res) {
+  let name = req.params.name;
+  let list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
+});
+
+router.get("/tweets/:id", function (req, res) {
+  let id = req.params.id;
+	let tweet = tweetBank.find( { id: id } );
+  res.render( 'index', { tweet: tweet });
+});
+
 module.exports = router;
